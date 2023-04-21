c7e2fba889ac80f464f3a1a693a5cbfe83f44303
MithosIV <101414251+MithosIV@users.noreply.github.com>
2023-04-20 19:55:07 -0600

    Collision wScreen

--- src/index.js
+++ src/index.js
@@ -26,25 +26,69 @@ new Phaser.Game(config);
 
 function preload () {
   this.load.image('space', 'assets/space.png');
-  this.load.image('player','assets/player.png');
+  this.load.image('player','assets/Ship.png');
+  this.load.image('bullet','assets/Bullet.png')
+  this.load.image('alien', 'assets/Enemy.png')
 }
 
 function create () {
   this.add.image(300, 300, 'space');
   player = this.add.sprite(config.width/2, 550,"player");
   this.physics.add.existing(player);
+  //alien = this.add.sprite(config.width/2, 50,"alien");
 
-  this.keys = this.input.keyboard.addKeys('A,D');
+  
+  //this.physics.add.existing(alien);
+
+  this.keys = this.input.keyboard.addKeys('A,D,SPACEBAR');
 
 }
 function update(){
+  if (this.keys.SPACEBAR.isDown)
+  {
+    shoot();
+  }
   if (this.keys.A.isDown){
-    player.body.velocity.x = -80;
+   movementL();
   }
   else if (this.keys.D.isDown){
-    player.body.velocity.x = 80;
+   movementR(); 
   }
   else{
     player.body.velocity.x = 0;
   }
+
+  
+}
+function movementL()
+{
+  if(player.body.x > 0)
+  {
+    player.body.velocity.x = -150;
+  }
+  else
+  {
+    player.body.velocity.x = 0;
+  }
+  
+}
+function movementR()
+{
+  if (player.body.x < config.width - player.width)
+    {
+      player.body.velocity.x = 150;
+    }
+    else
+    {
+      player.body.velocity.x = 0;
+    }
+  
+}
+function shoot()
+{
+  bullet = this.add.sprite(config.width/2,550,"bullet");
+  this.physics.add.existing(bullet);
+  pipe.body.allowGravity = false;
+  pipe.body.immovable = true;
+  bullet.body.velocity.y = 100;
 }
